642188e5bf1a9a7c56cd8abf41f6bd21a127dc8f
Tobias Schønau <tobias@schonau.dk>
2024-02-09 16:24:56 +0100

    a

--- server.py
+++ server.py
@@ -11,12 +11,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        print("Connected by addr")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            print(f"Received {data!r}")
+            print("Received data!r")
             if data == b'q':
                 break
             elif data == b'a':
